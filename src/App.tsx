32c53709fbb7803033aa0484b047b8a06065921a
Junaid <sj.shaikhjunaid@gmail.com>
2022-08-14 18:46:49 +0530

    set-up styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import { GlobalStyles } from "./components/styled/Global";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <GlobalStyles />
+    </div>
+  );
 }
 
 export default App;
